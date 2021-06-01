e0079ea9bdeb6d00fff0bbe2d526358892a948d4
Warun Sharma <warunsharma.95@gmail.com>
2021-06-01 06:52:35 +0530

    Added MongoStore for session storage in db

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 const passport = require('passport')
 const passportLocal = require('./config/passport-local-strategy')
+const MongoStore = require('connect-mongo');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
@@ -16,6 +17,8 @@ app.use(cookieParser())
 app.set('view engine','ejs')
 app.set('views','./views')
 
+
+
 app.use(session({
     name:'blogApp',
     secret:'changeLater',
@@ -23,8 +26,10 @@ app.use(session({
     resave:false,
     cookie:{
         maxAge:(1000*60*100)
-    }
-}));
+    },
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/BlogAppDb' })
+}
+    ));
 app.use(passport.initialize());
 app.use(passport.session());
 
